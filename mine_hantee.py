6eae7b7bdff545c19faf67aa2ec932d8d9067355
Agathe Puissant <52568496+AgathePuissant@users.noreply.github.com>
2019-11-08 11:33:45 +0100

    Update mine_hantee.py

--- mine_hantee.py
+++ mine_hantee.py
@@ -1,6 +1,25 @@
 from numpy import *
 
-
+class carte(object):
+    
+    def __init__(self, ID, orientation, deplacable=False, id_fantome = 0):
+        
+        self.id = ID
+        self.orientation = orientation
+        self.deplacable = deplacable
+        self.id_fantome = id_fantome
+        self.presence_pepite = True
+        
+        
+class joueur(object):
+    
+    def __init__(self, ID, nom, niveau = 0, fantome_target, position):
+        self.id = ID
+        self.nom = nom
+        self.niveau = niveau
+        self.fantome_target = fantome_target
+        self.position = position
+        self.points = 0  
 
 class plateau(object):
     def __init__(self, nb_joueurs, liste_noms, liste_niveaux, N=7):
