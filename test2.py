260be89275fcdeeb4d7a498d5cfc20d845a07666
eschalck <57525067+eschalck@users.noreply.github.com>
2019-12-13 10:56:59 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -596,13 +596,14 @@ class Bouton:
     
 class InputBox:
 
-    def __init__(self, x, y, w, h, text='', max_caract=10):
+    def __init__(self, x, y, w, h, text='', max_caract=10, contenu='texte'):
         self.rect = pygame.Rect(x, y, w, h)
         self.color = COLOR_INACTIVE
         self.text = text
         self.txt_surface = police2.render(text, True, self.color)
         self.active = False
         self.max_caract=max_caract
+        self.contenu=contenu
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -619,7 +620,9 @@ class InputBox:
             if self.active:
                 if event.key == pygame.K_BACKSPACE:
                     self.text = self.text[:-1]
-                elif len(self.text)<self.max_caract:
+                elif self.contenu=='num' and (event.key in [pygame.K_0,pygame.K_1,pygame.K_2,pygame.K_3,pygame.K_4,pygame.K_5,pygame.K_6,pygame.K_7,pygame.K_8,pygame.K_9,pygame.K_KP0,pygame.K_KP1,pygame.K_KP2,pygame.K_KP3,pygame.K_KP4,pygame.K_KP5,pygame.K_KP6,pygame.K_KP7,pygame.K_KP8,pygame.K_KP9]) and len(self.text)<self.max_caract:
+                    self.text += event.unicode
+                elif self.contenu=='texte' and len(self.text)<self.max_caract:
                     self.text += event.unicode
                 # Re-render le texte. 
                 self.txt_surface = police2.render(self.text, True, self.color)
@@ -1613,6 +1616,7 @@ police_small = pygame.font.Font("coda.ttf", 17) #Load font object.
 COLOR_INACTIVE = pygame.Color('lightskyblue3')
 COLOR_ACTIVE = pygame.Color('dodgerblue2')
 COLOR_ERROR = pygame.Color('tomato2')
+police1=pygame.font.SysFont('calibri', 15)
 police2=pygame.font.SysFont('calibri', 25)
 police3=pygame.font.SysFont('calibri', 35)
 fichier="mine_hantee_config.txt" 
