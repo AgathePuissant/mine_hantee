c89a173210decba01e2485b6155ab5f0ddfae0f0
eschalck <57525067+eschalck@users.noreply.github.com>
2020-01-04 10:34:51 +0100

    joueur 1 forcément manuel

--- main_version_objet.py
+++ main_version_objet.py
@@ -679,7 +679,9 @@ class mine_hantee():
                 choix_manuel=choix_modes_joueurs[k-1][0]
                 choix_automatique=choix_modes_joueurs[k-1][1]
                 choix_manuel.draw(self.fenetre)
-                choix_automatique.draw(self.fenetre)
+                #le joueur 1 est forcément manuel
+                if k!=1:
+                    choix_automatique.draw(self.fenetre)
                 #si le mode automatique est activé, l'utilisateur peut choisir le niveau de l'ia. 
                 #mais il ne peut pas choisir son nom
                 if choix_automatique.active:
@@ -709,7 +711,9 @@ class mine_hantee():
                     choix_manuel=choix_modes_joueurs[k-1][0]
                     choix_automatique=choix_modes_joueurs[k-1][1]
                     choix_manuel.handle_event(event,choix_modes_joueurs[k-1])
-                    choix_automatique.handle_event(event,choix_modes_joueurs[k-1])
+                    #le joueur 1 est forcément manuel
+                    if k!=1:
+                        choix_automatique.handle_event(event,choix_modes_joueurs[k-1])
                     if choix_automatique.active:
                         choix_lvl_joueurs[k-1][0].handle_event(event,choix_lvl_joueurs[k-1])
                         choix_lvl_joueurs[k-1][1].handle_event(event,choix_lvl_joueurs[k-1])
