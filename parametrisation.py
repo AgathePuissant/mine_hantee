6ac93fd4fe519cb69b7b6d3828b44e324106f01b
eschalck <57525067+eschalck@users.noreply.github.com>
2020-01-04 11:32:59 +0100

    +commentaires

--- parametrisation.py
+++ parametrisation.py
@@ -6,13 +6,15 @@ FICHIER DEFINISSANT LES FONCTIONS D'ACCES AU FICHIER DE PARAMETRISATION
 #définition du fichier de parametrisation
 fichier="mine_hantee_config.txt" 
 
-#lecture du fichier de paramétrage
-
 def lecture(fichier):
     """
-    fonction qui lit le fichier de paramètres
-    et retourne le dictionnaire des paramètres
-    (si l'utilisateur modifie directement le fichier des paramètres)
+    Fonction permettant de lire le fichier de paramétrage.  
+
+    Prend en entrée :
+        - fichier : nom du fichier texte de paramétrage (string, finissant par .txt). 
+
+    Renvoie un dictionnaire contenant l'ensemble des paramètres
+    lus dans le fichier.
     """
     f=open(fichier,"r")
     lignes=f.readlines()
@@ -24,13 +26,15 @@ def lecture(fichier):
             dico_parametres[ligne_coupe[0]]=ligne_coupe[1]
     return dico_parametres
 
-#écriture du fichier de paramétrage
-
 def ecriture(fichier, dico_parametres):
     """
-    fonction qui remplit le fichier de paramètres à partir du dictionnaire des nouveaux paramètres 
-    donnée en entrée dans dico_parametres. 
-    (si l'utilisateur modifie les paramètres vie l'interface de jeu (avancée ou non))
+    Fonction permettant de remplir / d'actualiser le fichier de paramétrage 
+    à partir d'un dictionnaire contenant les nouveaux paramètres.
+
+    Prend en entrée :
+        - fichier : nom du fichier texte de paramétrage (string, finissant par .txt). 
+        - dico_paramètres : dictionnaire contenant les paramètres à actualiser dans le 
+        fichier de paramétrage ainsi que leurs nouvelles valeurs (dictionnaire). 
     """
     f=open(fichier,"r")
     lignes=f.readlines()
@@ -60,9 +64,17 @@ def ecriture(fichier, dico_parametres):
 def enregistrement_inputs(dico):
     global fichier
     """
-    fonction d'enregistrement des paramètres inputs
-    dico : dictionnaire avec les paramètres, 
-    dont un paramètre (fonction) correspond à la fonction suivante appelée. 
+    Fonction permettant d'enregistrer dans le fichier de paramétrage
+    les nouveaux paramètres rentrés par l'utilisateur lors des différentes 
+    étapes de paramétrisation. 
+    enregistrement_inputs lance également, après l'enregistrement, les fonctions
+    suivantes (tests, suite de la paramétrisation ou jeu). 
+
+    Prend en entrée :
+        - dico : dictionnaire des paramètres entrés par l'utilisateur 
+        lors de la paramétrisation (dictionnaire). 
+        L'un des paramètres contenus dans ce dictionnaire (fonction)
+        correspond à la fonction suivante appelée après l'enregistrement. 
     """
     
     ecriture(fichier, dico)
@@ -77,23 +89,34 @@ def enregistrement_inputs(dico):
 
 def tests_max_inputs(dico):
     """
-    fonction qui teste la conformité des paramètres  renseignés par les joueurs. 
-    dico : dictionnaire avec les paramètres, 
-    dont un paramètre (fonction) correspond à la fonction suivante appelée.
-    dont un paramètre (fonction_prec) correspond à la fonction d'origine. 
-    Les types ne sont pas testés car ils sont forcés lors de l'entrée de l'utilisateur.
-    La fonction vérifie si les valeurs maximale ne sont pas dépassées. 
-    """
+    Fonction qui teste la conformité des paramètres renseignés par l'utilisateur. 
+    La fonction vérifie si les valeurs maximales des paramètres ne sont pas dépassées.
+    Pour deux paramètres (nb_fantomes et nb_fantomes_mission), elle vérifie également
+    les valeurs minimales. 
     
-    #dictionnaire des maximums autorisés pour chaque paramètre (s'il y en a).
+    Prend en entrée : 
+        - dico : dictionnaire des paramètres entrés par l'utilisateur 
+        lors de la paramétrisation (dictionnaire). 
+        Contient également : 
+            - un paramètre fonction correspond à la fonction suivante appelée.
+            - un paramètre fonction_prec correspond à la fonction d'origine.
+            - un paramètre test_max indiquant que les maximums n'ont pas encore été testés. 
+    
+    Crée un dictionnaire contenant les paramètres avec des erreurs ainsi que les erreurs associées. 
+    
+    Appelle la fonction suivante.
+    """
+    #initialisation du dictionnaire des maximums autorisés pour chaque paramètre (s'il y en a).
     n=int(dico['dimensions_plateau'])
     nb_fant=(n-2)*(n//2)+(n//2)*(n//2-1)
     dico_max={'nb_fantomes': nb_fant, 'nb_fantomes_mission': nb_fant//int(dico['nb_joueurs']), 
                 'nb_joker': 10, 'points_pepite': 50, 'points_fantome': 200, 
                 'points_fantome_mission': 200, 'bonus_mission': 500}
     
+    #initialisation du dictionnaire des minimums autorisés pour chaque paramètre (s'il y en a).
     dico_min={'nb_fantomes': int(dico['nb_joueurs'])*int(dico['nb_fantomes_mission']), 'nb_fantomes_mission':1}
     
+    #initialisation du dictionnaire contenant les erreurs et leurs types. 
     dico_erreurs={}
     
     for cle_input in dico.keys():
@@ -111,7 +134,8 @@ def tests_max_inputs(dico):
                 if int(dico[cle_input])<dico_min[cle_min]:
                     dico_erreurs[cle_input]='min'
                     erreur='min'
-                    
+    
+    #s'il n'y a pas d'erreurs, on appelle la fonction suivante. 
     if len(dico_erreurs)==0:
         dico['test_max']=False
         fonction_suivante=dico['fonction']
@@ -119,6 +143,9 @@ def tests_max_inputs(dico):
             fonction_suivante()
         else:
             fonction_suivante(dico)
+            
+    #s'il y a des erreurs, on appelle la fonction précédente, 
+    #en fournissant le dictionnaire des erreurs. 
     else:
         dico_erreurs['type']=erreur
         fonction_prec=dico['fonction_prec']
@@ -126,20 +153,28 @@ def tests_max_inputs(dico):
                         
 def tests_null_inputs(dico):
     """
-    fonction qui teste la conformité des paramètres  renseignés par les joueurs. 
-    dico : dictionnaire avec les paramètres, 
-    dont un paramètre (fonction) correspond à la fonction suivante appelée.
-    dont un paramètre (fonction_prec) correspond à la fonction d'origine. 
-    Les types ne sont pas testés car ils sont forcés lors de l'entrée de l'utilisateur.
-    La fonction vérifie que les inputsboxs ne sont pas vide et renvoie un message d'erreur si c'est le cas. 
-    """
+    Fonction qui teste la conformité des paramètres renseignés par l'utilisateur. 
+    La fonction vérifie si les inputsboxs de la paramétrisation ne sont pas vides. 
     
-    #liste qui renseigne les paramètres ne pouvant être nulls. 
+    Prend en entrée : 
+        - dico : dictionnaire des paramètres entrés par l'utilisateur 
+        lors de la paramétrisation (dictionnaire). 
+        Contient également : 
+            - un paramètre fonction correspond à la fonction suivante appelée.
+            - un paramètre fonction_prec correspond à la fonction d'origine.
+            - un paramètre test_null indiquant que la nullité n'a pas encore été testée. 
+    
+    Crée un dictionnaire contenant les paramètres avec des erreurs ainsi que les erreurs associées. 
+    
+    Appelle la fonction suivante.
+    """
+    #initialisation de la liste qui renseigne les paramètres ne pouvant être nulls. 
     #(seulement ceux pour lesquels il est possible que l'utilisateur entre une valeur nulle).
     L_null=['pseudo_joueur_1', 'pseudo_joueur_2','pseudo_joueur_3','pseudo_joueur_4',
             'nb_fantomes', 'nb_fantomes_mission','nb_joker', 'points_pepite', 
             'points_fantome', 'points_fantome_mission', 'bonus_mission']
     
+    #initialisation du dictionnaire contenant les erreurs et leurs types. 
     dico_erreurs={}
     
     for cle_input in dico.keys():
@@ -150,6 +185,7 @@ def tests_null_inputs(dico):
                 if len(dico[cle_input])==0:
                     dico_erreurs[cle_null]='null'
                     
+    #s'il n'y a pas d'erreurs, on appelle la fonction suivante.                 
     if len(dico_erreurs)==0:
         dico['test_null']=False
         if dico['test_max']==True:
@@ -158,6 +194,9 @@ def tests_null_inputs(dico):
         else:
             fonction_suite=dico['fonction']
             fonction_suite()
+            
+    #s'il y a des erreurs, on appelle la fonction précédente, 
+    #en fournissant le dictionnaire des erreurs. 
     else:
         dico_erreurs['type']='null'
         fonction_prec=dico['fonction_prec']
